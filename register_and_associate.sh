15e232f2257925dafd3acfcff5baa766efbe4046
Dmitry Rumiantsev <dmitry.rumiantsev@duallab.com>
2016-11-09 16:21:35 +0300

    Added '--list' option to script parameters

--- register_and_associate.sh
+++ register_and_associate.sh
@@ -1,31 +1,55 @@
 #!/bin/bash
-path=$1
+declare -A versionCommands
+declare -A checkersPresence
+declare -A checkersType
+
+while [ "$1" != "" ]; do
+  case $1 in
+    -p | --path)
+      shift
+      path=$1
+      ;;
+    -l | --list)
+      shift
+      listString=$1
+      IFS=';'
+      read -ra registeredCheckers <<< "$listString"
+      for checkerString in "${registeredCheckers[@]}"; do
+        trimmedString=${checkerString//[[:blank:]]/}
+        checkersName=${trimmedString%':'*}
+        checkersPresence[$checkersName]=true
+        checkersType[$checkersName]=${trimmedString#*':'}
+      done
+      IFS=$' \t\n'
+      ;;
+  esac
+  shift
+done
+
 if ! [[ -n "$path" ]]; then
   path="./"
 fi
 path=$path"*"
 
-declare -A versionCommands
-declare -A checkersPresence
-declare -A checkersType
-
 versionCommands["verapdf"]="verapdf --version"
 checkersType["verapdf"]="application/pdf"
 
 for key in "${!versionCommands[@]}";  do
-  commandString="${versionCommands[$key]}"
-  output=$(eval $commandString)
-  versionNumber="-1"
-  isVersionNumberDefined=false
-  while read -r line; do
-    if [ "$isVersionNumberDefined" = false ]; then
-      versionNumber=${line#*' '}
-      isVersionNumberDefined=true
-    fi
-  done <<<"$output"
-  if [ ${#versionNumber}-gt0 ]; then
-    if [[ $versionNumber =~ ^[0-9.]+$ ]]; then
-      checkersPresence[$key]=true
+  if ! (test "${checkersPresence[$key]+set}";) then
+    commandString="${versionCommands[$key]}"
+    output=$(eval $commandString)
+    versionNumber="-1"
+    isVersionNumberDefined=false
+    while read -r line; do
+      if [ "$isVersionNumberDefined" = false ]; then
+        versionNumber=${line#*' '}
+        isVersionNumberDefined=true
+      fi
+    done <<<"$output"
+    if [ ${#versionNumber} -gt 0 ]; then
+      if [[ $versionNumber =~ ^[0-9.]+$ ]]; then
+        checkersPresence[$key]=true
+      fi
     fi
   fi
 done
