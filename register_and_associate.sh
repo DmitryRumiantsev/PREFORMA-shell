1d2c053a5ee126e379dbe04204f2714ee945e804
Dmitry Rumiantsev <dmitry.rumiantsev@duallab.com>
2016-11-11 10:24:17 +0300

    Switched to using getopts - long commands are not supported. Refactored code

--- register_and_associate.sh
+++ register_and_associate.sh
@@ -3,68 +3,79 @@ declare -A versionCommands
 declare -A checkersPresence
 declare -A checkersType
 
-while [ "$1" != "" ]; do
-  case $1 in
-    -p | --path)
-      shift
-      path=$1
-      ;;
-    -l | --list)
-      shift
-      listString=$1
-      IFS=';'
-      read -ra registeredCheckers <<< "$listString"
-      for checkerString in "${registeredCheckers[@]}"; do
-        trimmedString=${checkerString//[[:blank:]]/}
-        checkersName=${trimmedString%':'*}
-        checkersPresence[$checkersName]=true
-        checkersType[$checkersName]=${trimmedString#*':'}
-      done
-      IFS=$' \t\n'
-      ;;
-  esac
-  shift
-done
+path="./"
 
-if ! [[ -n "$path" ]]; then
-  path="./"
-fi
-path=$path"*"
+function checkOptions (){
+  while getopts ":p:l:" option; do
+    case $option in
+      p)
+        path=$OPTARG
+        ;;
+      l)
+        parseCheckersInfo "$OPTARG"
+        ;;
+    esac
+  done
+  path=$path"*"
+}
 
-versionCommands["verapdf"]="verapdf --version"
-checkersType["verapdf"]="application/pdf"
+function parseCheckersInfo {
+  IFS=';'
+  read -ra registeredCheckers <<< "$1"
+  for checkerString in "${registeredCheckers[@]}"; do
+    trimmedString=${checkerString//[[:blank:]]/}
+    checkersName=${trimmedString%':'*}
+    checkersPresence[$checkersName]=true
+    checkersType[$checkersName]=${trimmedString#*':'}
+  done
+  IFS=$' \t\n'
+}
 
-for key in "${!versionCommands[@]}";  do
-  if ! (test "${checkersPresence[$key]+set}";) then
-    commandString="${versionCommands[$key]}"
-    output=$(eval $commandString)
-    versionNumber="-1"
-    isVersionNumberDefined=false
-    while read -r line; do
-      if [ "$isVersionNumberDefined" = false ]; then
-        versionNumber=${line#*' '}
-        isVersionNumberDefined=true
-      fi
-    done <<<"$output"
-    if [ ${#versionNumber} -gt 0 ]; then
-      if [[ $versionNumber =~ ^[0-9.]+$ ]]; then
-        checkersPresence[$key]=true
-      fi
-    fi
-  fi
-done
-
-for file in $(find $path -type f); do
-    typeString=$(mimetype --output-format %m "$file")
-    isCheckerDefined=false
-    for key in "${!checkersType[@]}";  do
-        checkersTypeString=${checkersType[$key]}
-        if [[ "$checkersTypeString" = *"$typeString"* ]] && [ "${checkersPresence[$key]}" = true ]; then
-          echo $file": "$key
-          isCheckerDefined=true
+function checkPresence (){
+  for key in "${!versionCommands[@]}";  do
+    if ! (test "${checkersPresence[$key]+set}";) then
+      commandString="${versionCommands[$key]}"
+      output=$(eval $commandString)
+      versionNumber="-1"
+      isVersionNumberDefined=false
+      while read -r line; do
+        if [ "$isVersionNumberDefined" = false ]; then
+          versionNumber=${line#*' '}
+          isVersionNumberDefined=true
         fi
-    done
-    if [ "$isCheckerDefined" = false ]; then
-      echo $file": checker unassigned"
+      done <<<"$output"
+      if [ ${#versionNumber} -gt 0 ]; then
+        if [[ $versionNumber =~ ^[0-9.]+$ ]]; then
+          checkersPresence[$key]=true
+        fi
+      fi
     fi
-done
+  done
+}
+
+function associateFilesWithCheckers (){
+  for file in $(find $path -type f); do
+      typeString=$(mimetype --output-format %m "$file")
+      isCheckerDefined=false
+      for key in "${!checkersType[@]}";  do
+          checkersTypeString=${checkersType[$key]}
+          if [[ "$checkersTypeString" = *"$typeString"* ]] && [ "${checkersPresence[$key]}" = true ]; then
+            echo $file": "$key
+            isCheckerDefined=true
+          fi
+      done
+      if [ "$isCheckerDefined" = false ]; then
+        echo $file": checker unassigned"
+      fi
+  done
+}
+
+function initMaps (){
+  versionCommands["verapdf"]="verapdf --version"
+  checkersType["verapdf"]="application/pdf"
+}
+
+checkOptions "$@"
+initMaps
+checkPresence
+associateFilesWithCheckers
